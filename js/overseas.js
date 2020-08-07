02698b5f8396699ae383706e8645314bf393ce2a
Shuyang He <maxhe2019@gmail.com>
2020-08-07 12:50:27 -0700

    update 08/07

--- js/overseas.js
+++ js/overseas.js
@@ -8,4 +8,124 @@
     },
     false
   );
+
+  document.querySelector("#nav-links-home").addEventListener(
+    "mouseover",
+    (event) => {
+      document.querySelector("#nav-links-home > span").style.color = "#4272d7";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-home").addEventListener(
+    "mouseout",
+    (event) => {
+      document.querySelector("#nav-links-home > span").style.color = "#505050";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-solution").addEventListener(
+    "mouseover",
+    (event) => {
+      document.querySelector("#nav-links-solution > span").style.color =
+        "#4272d7";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-solution").addEventListener(
+    "mouseout",
+    (event) => {
+      document.querySelector("#nav-links-solution > span").style.color =
+        "#505050";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-jobs").addEventListener(
+    "mouseover",
+    (event) => {
+      document.querySelector("#nav-links-jobs > span").style.color = "#4272d7";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-jobs").addEventListener(
+    "mouseout",
+    (event) => {
+      document.querySelector("#nav-links-jobs > span").style.color = "#505050";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-business").addEventListener(
+    "mouseover",
+    (event) => {
+      document.querySelector("#nav-links-business > span").style.color =
+        "#4272d7";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-business").addEventListener(
+    "mouseout",
+    (event) => {
+      document.querySelector("#nav-links-business > span").style.color =
+        "#505050";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-education").addEventListener(
+    "mouseover",
+    (event) => {
+      document.querySelector("#nav-links-education > span").style.color =
+        "#4272d7";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-education").addEventListener(
+    "mouseout",
+    (event) => {
+      document.querySelector("#nav-links-education > span").style.color =
+        "#505050";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-news").addEventListener(
+    "mouseover",
+    (event) => {
+      document.querySelector("#nav-links-news > span").style.color = "#4272d7";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-news").addEventListener(
+    "mouseout",
+    (event) => {
+      document.querySelector("#nav-links-news > span").style.color = "#505050";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-about-us").addEventListener(
+    "mouseover",
+    (event) => {
+      document.querySelector("#nav-links-about-us > span").style.color =
+        "#4272d7";
+    },
+    false
+  );
+
+  document.querySelector("#nav-links-about-us").addEventListener(
+    "mouseout",
+    (event) => {
+      document.querySelector("#nav-links-about-us > span").style.color =
+        "#505050";
+    },
+    false
+  );
 })();
